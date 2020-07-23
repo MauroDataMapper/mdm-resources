266c8a08c84d283f8c2ee68dab12aa16925663a8
Pete <peter.gammon@oxfordcc.co.uk>
2020-07-23 16:09:18 +0100

    bumped version 4.1.5

--- lib/es2015/mdm-session.resource.d.ts
+++ lib/es2015/mdm-session.resource.d.ts
@@ -9,4 +9,5 @@ export declare class MdmSessionResource extends MdmResource {
     keepAlive(queryStringParams?: any, restHandlerOptions?: any): any;
     activeSessions(queryStringParams?: any, restHandlerOptions?: any): any;
     isAuthenticated(sessionId?: any, queryStringParams?: any, restHandlerOptions?: any): any;
+    isApplicationAdministration(queryStringParams?: any, restHandlerOptions?: any): any;
 }
