01e5eff827a5f15fabddcd82a405005b800cdf34
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2021-02-02 14:57:30 +0000

    MC-9313 Add additional APIs to manage subscribed models

--- src/mdm-subscribed-catalogues.resource.ts
+++ src/mdm-subscribed-catalogues.resource.ts
@@ -26,30 +26,130 @@ import { IMdmQueryStringParams, IMdmRestHandlerOptions } from "./mdm-rest-handle
  |   POST   | /api/subscribedCatalogues                                                                                                     | Action: save
  |   PUT    | /api/subscribedCatalogues/${id}                                                                                               | Action: update
  |  DELETE  | /api/subscribedCatalogues/${id}                                                                                               | Action: delete
+ |   GET    | /api/subscribedCatalogues/${id}/availableModels                                                                               | Action: index
+ |   GET    | /api/subscribedCatalogues/${id}/subscribedModels                                                                              | Action: index
+ |   GET    | /api/subscribedCatalogues/${catalogId}/subscribedModels/${id}                                                                 | Action: show
+ |   POST   | /api/subscribedCatalogues/${catalogId}/subscribedModels                                                                       | Action: save
+ |  DELETE  | /api/subscribedCatalogues/${catalogId}/subscribedModels/${id}                                                                 | Action: delete
  */
 export class MdmSubscribedCataloguesResource extends MdmResource {
+    /**
+     * Gets a Subscribed Catalogue by ID.
+     * @param id The UUID of the Subscribed Catalogue to get.
+     * @param queryStringParams Optional query string parameters for the GET request.
+     * @param restHandlerOptions Optional REST handler parameters.
+     * @returns An HTTP response object containing the data.
+     */
     get(id: string, queryStringParams?: IMdmQueryStringParams, restHandlerOptions?: IMdmRestHandlerOptions) {
         const url = `${this.apiEndpoint}/subscribedCatalogues/${id}`;
         return this.simpleGet(url, queryStringParams, restHandlerOptions);
     }
 
+    /**
+     * Gets a list of all Subscribed Catalogues.
+     * @param queryStringParams Optional query string parameters for the GET request.
+     * @param restHandlerOptions Optional REST handler parameters.
+     * @returns An HTTP response object containing a count and array of items.
+     */
     list(queryStringParams?: IMdmQueryStringParams, restHandlerOptions?: IMdmRestHandlerOptions) {
         const url = `${this.apiEndpoint}/subscribedCatalogues`;
         return this.simpleGet(url, queryStringParams, restHandlerOptions);
     }
 
+    /**
+     * Creates a new Subscribed Catalogue.
+     * @param data The data to use for creation.
+     * @param restHandlerOptions Optional REST handler parameters.
+     * @returns An HTTP response object.
+     */
     save(data: any, restHandlerOptions?: IMdmRestHandlerOptions) {
         const url = `${this.apiEndpoint}/subscribedCatalogues`;
         return this.simplePost(url, data, restHandlerOptions);
     }
 
+    /**
+     * Updates an existing Subscribed Catalogue.
+     * @param id The UUID of the Subscribed Catalogue to update.
+     * @param data The data to use for the update.
+     * @param restHandlerOptions Optional REST handler parameters.
+     * @returns An HTTP response object.
+     */
     update(id: string, data: any, restHandlerOptions?: IMdmRestHandlerOptions) {
         const url = `${this.apiEndpoint}/subscribedCatalogues/${id}`;
         return this.simplePut(url, data, restHandlerOptions);
     }
 
+    /**
+     * Removes an existing Subscribed Catalogue.
+     * @param id The UUID of the Subscribed Catalogue to remove.
+     * @param queryStringParams Optional query string parameters for the GET request.
+     * @param restHandlerOptions Optional REST handler parameters.
+     * @returns An HTTP response object.
+     */
     remove(id: string, queryStringParams?: IMdmQueryStringParams, restHandlerOptions?: IMdmRestHandlerOptions) {
         const url = `${this.apiEndpoint}/subscribedCatalogues/${id}`;
+        return this.simpleDelete(url, queryStringParams, restHandlerOptions);
+    }
+
+    /**
+     * Gets a list of all available federated models from a Subscribed Catalogue.
+     * @param id The UUID of the Subscribed Catalogue to search in.
+     * @param queryStringParams Optional query string parameters for the GET request.
+     * @param restHandlerOptions Optional REST handler parameters.
+     * @returns An HTTP response object containing a count and array of items.
+     */
+    listAvailableModels(id: string, queryStringParams?: IMdmQueryStringParams, restHandlerOptions?: IMdmRestHandlerOptions) {
+        const url = `${this.apiEndpoint}/subscribedCatalogues/${id}/availableModels`;
+        return this.simpleGet(url, queryStringParams, restHandlerOptions);
+    }
+
+    /**
+     * Gets a list of all federated models that this catalogue has subscribed to from a Subscribed Catalogue.
+     * @param id The UUID of the Subscribed Catalogue to search in.
+     * @param queryStringParams Optional query string parameters for the GET request.
+     * @param restHandlerOptions Optional REST handler parameters.
+     * @returns An HTTP response object containing a count and array of items.
+     */
+    listSubscribedModels(id: string, queryStringParams?: IMdmQueryStringParams, restHandlerOptions?: IMdmRestHandlerOptions) {
+        const url = `${this.apiEndpoint}/subscribedCatalogues/${id}/subscribedModels`;
+        return this.simpleGet(url, queryStringParams, restHandlerOptions);
+    }
+
+    /**
+     * Gets a Subscribed Catalogue by ID.
+     * @param catalogueId The UUID of the Subscribed Catalogue to search in.
+     * @param id The UUID of the subscription to get.
+     * @param queryStringParams Optional query string parameters for the GET request.
+     * @param restHandlerOptions Optional REST handler parameters.
+     * @returns An HTTP response object containing the data.
+     */
+    getSubscribedModel(catalogueId: string, id: string, queryStringParams?: IMdmQueryStringParams, restHandlerOptions?: IMdmRestHandlerOptions) {
+        const url = `${this.apiEndpoint}/subscribedCatalogues/${catalogueId}/subscribedModels/${id}`;
+        return this.simpleGet(url, queryStringParams, restHandlerOptions);
+    }
+
+    /**
+     * Creates a new subscription to a model in a Subscribed Catalogue.
+     * @param catalogueId The UUID of the Subscribed Catalogue to save to.
+     * @param data The data to use for creation.
+     * @param restHandlerOptions Optional REST handler parameters.
+     * @returns An HTTP response object.
+     */
+    saveSubscribedModel(catalogueId: string, data: any, restHandlerOptions?: IMdmRestHandlerOptions) {
+        const url = `${this.apiEndpoint}/subscribedCatalogues/${catalogueId}/subscribedModels`;
+        return this.simplePost(url, data, restHandlerOptions);
+    }
+
+    /**
+     * Removes an existing subscription to a model in a Subscribed Catalogue.
+     * @param catalogueId The UUID of the Subscribed Catalogue to remove from.
+     * @param id The UUID of the subscribed model to remove.
+     * @param queryStringParams Optional query string parameters for the GET request.
+     * @param restHandlerOptions Optional REST handler parameters.
+     * @returns An HTTP response object.
+     */
+    removeSubscribedModel(catalogueId: string, id: string, queryStringParams?: IMdmQueryStringParams, restHandlerOptions?: IMdmRestHandlerOptions) {
+        const url = `${this.apiEndpoint}/subscribedCatalogues/${catalogueId}/subscribedModels/${id}`;
         return this.simpleDelete(url, queryStringParams, restHandlerOptions);
     }
 }
