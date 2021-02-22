b09c7163e0130387e280d7c58183828bbf94f4bb
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2021-02-22 14:31:08 +0000

    MC-9388 Fix import paths

--- src/mdm-api-properties.ts
+++ src/mdm-api-properties.ts
@@ -16,8 +16,8 @@ limitations under the License.
 SPDX-License-Identifier: Apache-2.0
 */
 
-import { MdmResource } from "mdm-resource"
-import { IMdmQueryStringParams, IMdmRestHandlerOptions } from "mdm-rest-handler";
+import { MdmResource } from "./mdm-resource"
+import { IMdmQueryStringParams, IMdmRestHandlerOptions } from "./mdm-rest-handler";
 
 export class MdmApiPropertyResources extends MdmResource {
 
