1e4009e86ae457cca0810e06058d615be5b06d10
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2021-03-16 15:49:14 +0000

    Install TypeDoc and apply basic doc comments to common types

--- src/index.ts
+++ src/index.ts
@@ -60,4 +60,4 @@ export * from './mdm-resource';
 export * from './mdm-versioning.resource';
 export * from './mdm-profile.resource';
 export * from './mdm-subscribed-catalogues.resource';
-export * from './mdm-api-properties';
+export * from './mdm-api-properties.resource';
