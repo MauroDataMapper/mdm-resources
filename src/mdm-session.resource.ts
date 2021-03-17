e6dd59d952c446a3b99285b9a1b5d26c76fc4b86
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2021-03-17 09:25:38 +0000

    More updates to doc comments

--- src/mdm-session.resource.ts
+++ src/mdm-session.resource.ts
@@ -19,12 +19,9 @@ import { MdmResource } from './mdm-resource';
 import { IMdmQueryStringParams, IMdmRestHandlerOptions } from './mdm-rest-handler';
 
 /**
- * Controller: session
- |   GET    | /api/session/keepAlive                                                                                | Action: keepAlive
- |   GET    | /api/admin/activeSessions                                                                             | Action: activeSessions
- |   GET    | /api/session/isAuthenticated/${sesssionId}?                                                           | Action: isAuthenticatedSession
+ * MDM resource for managing sessions.
  */
-export class MdmSessionResource extends MdmResource {
+export class MdmSessionResource extends MdmResource {    
     keepAlive(queryStringParams?: IMdmQueryStringParams, restHandlerOptions?: IMdmRestHandlerOptions) {
         const url = `${this.apiEndpoint}/session/keepAlive`;
         return this.simpleGet(url, queryStringParams, restHandlerOptions);
@@ -35,13 +32,41 @@ export class MdmSessionResource extends MdmResource {
         return this.simpleGet(url, queryStringParams, restHandlerOptions);
     }
 
+    /**
+     * `HTTP GET` - Confirm if a session has been authenticated.
+     * @param sessionId An optional UUID representing a session to test. If not provided, the current session will be tested.
+     * @param queryStringParams Optional query string parameters, if required.
+     * @param restHandlerOptions Optional REST handler options, if required.
+     * @returns The result of the `GET` request.
+     * 
+     * On success, the response will be a `200 OK` and contain a response body similar to below:
+     * 
+     * ```ts
+     * {
+     *  authenticatedSession: true
+     * }
+     * ```
+     */
     isAuthenticated(sessionId?: string, queryStringParams?: IMdmQueryStringParams, restHandlerOptions?: IMdmRestHandlerOptions) {
         const url = `${this.apiEndpoint}/session/isAuthenticated${sessionId ? `/${sessionId}` : ''}`;
         return this.simpleGet(url, queryStringParams, restHandlerOptions);
     }
 
-    isApplicationAdministration(queryStringParams?: IMdmQueryStringParams, restHandlerOptions?: IMdmRestHandlerOptions)
-    {
+    /**
+     * `HTTP GET` - Confirm if the current authenticated session has administrative priviledges.
+     * @param queryStringParams Optional query string parameters, if required.
+     * @param restHandlerOptions Optional REST handler options, if required.
+     * @returns The result of the `GET` request.
+     * 
+     * On success, the response will be a `200 OK` and contain a response body similar to below:
+     * 
+     * ```ts
+     * {
+     *  applicationAdministrationSession: true
+     * }
+     * ```
+     */
+    isApplicationAdministration(queryStringParams?: IMdmQueryStringParams, restHandlerOptions?: IMdmRestHandlerOptions) {
         const url = `${this.apiEndpoint}/session/isApplicationAdministration`;
         return this.simpleGet(url, queryStringParams, restHandlerOptions);
     }
