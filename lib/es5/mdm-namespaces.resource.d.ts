8a0561367b2038a6d06842d951128b34b5be4248
Oliver Freeman <oliver.freeman@bdi.ox.ac.uk>
2021-09-08 16:02:57 +0100

    Remove the lib directory from git

--- lib/es5/mdm-namespaces.resource.d.ts
+++
@@ -1,9 +0,0 @@
-import { RequestSettings, QueryParameters } from './mdm-common.model';
-import { MdmResource } from './mdm-resource';
-/**
- * Controller: metadata
- |   GET    | /api/metadata/namespaces/${id}?                                                                       | Action: namespaces
- */
-export declare class MdmNamespacesResource extends MdmResource {
-    get(metadataId?: string, queryStringParams?: QueryParameters, restHandlerOptions?: RequestSettings): any;
-}
