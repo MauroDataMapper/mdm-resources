e1404ee39ce02f2a5b9c621932653a81d9e6305b
Pete <peter.gammon@oxfordcc.co.uk>
2021-12-29 13:45:52 +0000

    correcting lint issues

--- src/mdm-subscribed-catalogues.resource.ts
+++ src/mdm-subscribed-catalogues.resource.ts
@@ -114,15 +114,15 @@ export class MdmSubscribedCataloguesResource extends MdmResource {
   }
 
   /**
- * `HTTP GET` - Gets a list of all published models from a Subscribed Catalogue.
- *
- * @param id The UUID of the Subscribed Catalogue to search in.
- * @param query Optional query string parameters for the GET request.
- * @param options Optional REST handler parameters.
- * @returns The result of the `GET` request.
- *
- * `200 OK` - will return a {@link PublishedDataModelIndexResponse} containing a list of {@link PublishedDataModel} items.
- */
+   * `HTTP GET` - Gets a list of all published models from a Subscribed Catalogue.
+   *
+   * @param id The UUID of the Subscribed Catalogue to search in.
+   * @param query Optional query string parameters for the GET request.
+   * @param options Optional REST handler parameters.
+   * @returns The result of the `GET` request.
+   *
+   * `200 OK` - will return a {@link PublishedDataModelIndexResponse} containing a list of {@link PublishedDataModel} items.
+   */
   listPublishedModels(id: Uuid, query?: QueryParameters, options?: RequestSettings) {
     const url = `${this.apiEndpoint}/subscribedCatalogues/${id}/publishedModels`;
     return this.simpleGet(url, query, options);
@@ -149,7 +149,7 @@ export class MdmSubscribedCataloguesResource extends MdmResource {
    * @param id The unique identifier of the Subscribed Model to federate.
    * @param options Optional REST handler parameters.
    * @returns The result of the `POST` request.
-   * @deprecated no longer in use, perfomed as part of save operation 
+   * @deprecated no longer in use, perfomed as part of save operation
    *
    * `200 OK` - will return a {@link SubscribedDataModelResponse} containing a {@link SubscribedDataModel}.
    */
@@ -206,31 +206,31 @@ export class MdmSubscribedCataloguesResource extends MdmResource {
   }
 
   /**
- * `HTTP GET` - Tests subscribed catalogue URL.
- *
- * @param id The unique identifier of the Subscribed Catalogue to get.
- * @param query Optional query string parameters for the GET request.
- * @param options Optional REST handler parameters.
- * @returns The result of the `GET` request.
- *
- * `200 OK` - will return a blank success.
- */
+   * `HTTP GET` - Tests subscribed catalogue URL.
+   *
+   * @param id The unique identifier of the Subscribed Catalogue to get.
+   * @param query Optional query string parameters for the GET request.
+   * @param options Optional REST handler parameters.
+   * @returns The result of the `GET` request.
+   *
+   * `200 OK` - will return a blank success.
+   */
   testConnection(id: Uuid, query?: QueryParameters, options?: RequestSettings) {
     const url = `${this.apiEndpoint}/subscribedCatalogues/${id}/testConnection`;
     return this.simpleGet(url, query, options);
   }
 
   /**
-* `HTTP GET` - Get all newer versions of current model.
-*
-* @param subscribedCatalogueId The unique identifier of the Subscribed Catalogue to get.
-* @param publishedModelId The unique identifier of the Published Model to get.
-* @param query Optional query string parameters for the GET request.
-* @param options Optional REST handler parameters.
-* @returns The result of the `GET` request.
-*
-* `200 OK` - will return a list of all newer models.
-*/
+   * `HTTP GET` - Get all newer versions of current model.
+   *
+   * @param subscribedCatalogueId The unique identifier of the Subscribed Catalogue to get.
+   * @param publishedModelId The unique identifier of the Published Model to get.
+   * @param query Optional query string parameters for the GET request.
+   * @param options Optional REST handler parameters.
+   * @returns The result of the `GET` request.
+   *
+   * `200 OK` - will return a list of all newer models.
+   */
   newerVersions(subscribedCatalogueId: Uuid, publishedModelId: Uuid, query?: QueryParameters, options?: RequestSettings) {
     const url = `${this.apiEndpoint}/subscribedCatalogues/${subscribedCatalogueId}/publishedModels/${publishedModelId}/newerVersions`;
     return this.simpleGet(url, query, options);
