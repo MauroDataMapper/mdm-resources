c3e4e5ebb09e6ddeb78ca78dc595047a9de5a7fc
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2022-04-27 11:12:20 +0100

    MC-9812 Update profile resource to include search endpoints

--- src/mdm-profile.model.ts
+++ src/mdm-profile.model.ts
@@ -17,7 +17,7 @@ limitations under the License.
 SPDX-License-Identifier: Apache-2.0
 */
 
-import { CatalogueItemDomainType, MdmIndexResponse, MdmResponse, Payload, Uuid, Version } from './mdm-common.model';
+import { Breadcrumb, CatalogueItemDomainType, MdmIndexResponse, MdmResponse, Payload, Uuid, Version } from './mdm-common.model';
 
 export type ProfileFieldDataType =
   'boolean'
@@ -138,3 +138,23 @@ export interface Metadata {
 export type MetadataIndexResponse = MdmIndexResponse<Metadata>;
 
 export type ProfilePayload = Profile & Payload;
+
+export interface ProfileSearchResultField {
+  [key: string]: any;
+  fieldName: string;
+  metadataPropertyName: string;
+  currentValue?: string;
+  dataType: ProfileFieldDataType;
+  allowedValues?: string[];
+}
+
+export interface ProfileSearchResult {
+  [key: string]: any;
+  id: Uuid;
+  label: string;
+  description?: string;
+  breadcrumbs: Breadcrumb[];
+  profileFields: ProfileSearchResultField[];
+}
+
+export type ProfileSearchResponse = MdmIndexResponse<ProfileSearchResult>;
